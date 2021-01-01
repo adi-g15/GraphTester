38281505f21f4a967684ffdac5dc11dddde60869
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2021-01-01 13:57:10 +0530

    Add original test.cxx content from graphMat

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+#define TEST_WITH_OBJECT_MATRIX true
+#define OLD_TEST false
+
+
+#if TEST_WITH_OBJECT_MATRIX
 #include "3d_graph_mat.hpp"
 #include <list>
 #include <atomic>
@@ -53,10 +58,68 @@ int main() {
 	//std::cout << std::boolalpha << std::is_invocable_r_v<int, decltype(func3), int, int, int>;	// false
 	//std::cout << std::boolalpha << std::is_invocable_r_v<int, decltype(func3), int, int>;	// false
 
-	matrix.init([](BoxObject& box) {
+	//matrix.init([](BoxObject& box) {
 		//box.entities = { nullptr, nullptr, nullptr };
 		//box.id = std::rand();
 		//box.name = "Some name";
-	});
+	//});
 	system("pause");
 }
+
+#elif OLD_TEST
+#include <iostream>
+
+#include "graph_square_mat.hpp"
+#include "3d_graph_mat.hpp"
+
+using namespace std;
+
+void func(Graph_Matrix_3D<int>& mat) {
+	static int count = 1;
+	std::cout << "Called " << count++ << "th time: \n";
+	//for (int i = mat.min_z; i <= mat.max_z; i++)
+	//{
+	//	mat.disp_xy_layer(i);	// debug
+	//	std::cout << "\n\n";	// debug
+	//}
+}
+
+int main(int argc, char const* argv[])
+{
+#if 0
+	int init_rows{ 4 }, init_cols{ 8 }, final_rows{ 3 }, final_cols{ 15 };
+	Graph_Matrix<int> mat(init_rows, init_cols);
+	mat.displayMat();
+	mat.resize(final_rows, final_cols);
+	mat.displayMat();
+#endif
+
+#if 1
+	Graph_Matrix_3D<int> mat;
+
+	mat.resize({ 3,3,3 });
+	mat.init([](int x, int y, int z) {	return x + y + z;	});
+	func(mat);
+	//mat.pop_xminus_layer();
+	//func(mat);
+	//mat.pop_xplus_layer();
+
+	//func(mat);
+	//mat.pop_yminus_layer();	// @problem
+	//func(mat);
+	//mat.pop_yplus_layer();
+
+	//func(mat);
+	//mat.pop_zplus_layer();
+	//func(mat);
+	//mat.resize({ 4,6,5 });
+	//mat.init([](int x, int y, int z) {	return x + y + z;	});
+	//func(mat);
+	//mat.pop_zminus_layer();
+	func(mat);
+
+#endif
+
+	return 0;
+}
+#endif
